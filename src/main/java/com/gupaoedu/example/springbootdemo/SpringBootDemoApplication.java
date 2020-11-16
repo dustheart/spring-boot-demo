8b97ec40deeb7e62669cf7f3612fd68d6a434a98
juan.wang <juan.wang@itrigger.com>
2020-11-16 22:28:56 +0800

    集成mybatis：添加MapperScan和mybatis.mapper-locations的配置

--- src/main/java/com/gupaoedu/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/gupaoedu/example/springbootdemo/SpringBootDemoApplication.java
@@ -1,10 +1,12 @@
 package com.gupaoedu.example.springbootdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.gupaoedu.example")
+@MapperScan("com.gupaoedu.example.dao.mappers")
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
