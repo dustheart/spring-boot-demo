adb8629caa42e8ef2514cfe5a75f4281169beba0
juan.wang <juan.wang@itrigger.com>
2020-11-16 21:23:04 +0800

    使用@Value

--- src/main/java/com/gupaoedu/example/controller/HelloController.java
+++ src/main/java/com/gupaoedu/example/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.gupaoedu.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${hello}")
+	private String name;
+
 	@GetMapping
 	public String test(){
-		return "Hello Spring Boot";
+		return "Hello Spring Boot " + name;
 	}
 
 }
