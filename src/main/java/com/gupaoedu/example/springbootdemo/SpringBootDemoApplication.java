c16fdce711bdfc31ddcf33a32237ed7247ceb101
juan.wang <juan.wang@itrigger.com>
2020-11-16 20:53:34 +0800

    添加Controller

--- src/main/java/com/gupaoedu/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/gupaoedu/example/springbootdemo/SpringBootDemoApplication.java
@@ -2,12 +2,20 @@ package com.gupaoedu.example.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String test(){
+		return "Hello Spring Boot";
 	}
 
 }
