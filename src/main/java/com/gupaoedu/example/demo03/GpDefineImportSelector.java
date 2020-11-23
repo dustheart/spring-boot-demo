55a3d492d65aa41ced965348c97ca83b66af0b5a
juan.wang <juan.wang@itrigger.com>
2020-11-23 22:36:17 +0800

    ImportSelector，可以是配置类，也可以是bean

--- src/main/java/com/gupaoedu/example/demo03/GpDefineImportSelector.java
+++ src/main/java/com/gupaoedu/example/demo03/GpDefineImportSelector.java
@@ -1,6 +1,6 @@
 package com.gupaoedu.example.demo03;
 
-import com.gupaoedu.example.demo01.RedisConfiguration;
+import com.gupaoedu.example.demo01.GpRedisTemplate;
 import com.gupaoedu.example.demo02.MybatisConfiguration;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
@@ -9,7 +9,9 @@ public class GpDefineImportSelector implements ImportSelector {
 
 	@Override
 	public String[] selectImports(AnnotationMetadata annotationMetadata) {
-		return new String[]{RedisConfiguration.class.getName(), MybatisConfiguration.class.getName()};
+		// 动态导入bean，告诉了spring，两个配置类在哪里
+		// 可以是配置类，也可以是bean
+		return new String[]{GpRedisTemplate.class.getName(), MybatisConfiguration.class.getName()};
 	}
 
 }
